d9f4b4ad179b77a011a3f818402bf7eddf67d109
jdb <mail@jdb.sh>
2020-05-16 10:12:46 +0100

    Remove nginject

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,19 +1,19 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 
 import { DepartureBoardIOQuery, DepartureBoardIOOptions } from './types';
 
 export class DataSource extends DataSourceWithBackend<DepartureBoardIOQuery, DepartureBoardIOOptions> {
-  /** @ngInject */
-  constructor(instanceSettings: DataSourceInstanceSettings<DepartureBoardIOOptions>, private templateSrv: any) {
+  constructor(instanceSettings: DataSourceInstanceSettings<DepartureBoardIOOptions>) {
     super(instanceSettings);
   }
 
   // Support template variables for stationCRS.
   applyTemplateVariables(query: DepartureBoardIOQuery) {
+    const templateSrv = getTemplateSrv();
     return {
       ...query,
-      stationCRS: this.templateSrv.replace(query.stationCRS),
+      stationCRS: query.stationCRS ? templateSrv.replace(query.stationCRS) : '',
     };
   }
 }
